0f398bb8419b4ea4057be94e9ca61aae26cf3555
Mel Maki <melody.rackham@gmail.com>
2019-12-04 13:53:51 -0700

    Server.js built out

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+server.use(logger);
+const router = require('./users/userRouter');
+server.use('./api/users', router);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+  next();
 });
 
 //custom middleware
